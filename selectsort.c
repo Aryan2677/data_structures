ea8958eaef723e01cc9a9cbd346d8e59b7636d36
Aryan2677 <aryandhiman77@gmail.com>
2019-08-09 10:46:15 +0530

    selection sorting of array

--- selectsort.c
+++ selectsort.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(int argc, const char* argv[])
 {
-int i,j,temp=0;
+int i,j,temp=0,pos=0;
 int arr[argc-1];
 int n=argc-1;
 for(i=1;i<argc;i++)
@@ -11,16 +11,20 @@ arr[i-1]=atoi(argv[i]);
 
 for(i=0;i<n;i++)
 {
-j=0;
+pos=i;
 for(j=i+1;j<n;j++)
 {
-if(arr[i]>arr[j])
+if(arr[pos]>arr[j]){
+pos=j;
+}
+}
+if(pos!=i)
 { 
 temp=arr[i];
-arr[i]=arr[j];
-arr[j]=temp;
+arr[i]=arr[pos];
+arr[pos]=temp;
 }
-}
+
 }
 for(i=0;i<n;i++)
 {
